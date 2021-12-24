e121bc4ba818bac856253e88430f4f8a1bf5561d
jcaswell <jakecaswell88@gmail.com>
2021-12-24 02:11:25 -0500

    created the user model

--- models/User.js
+++ models/User.js
@@ -2,10 +2,50 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   // build schema here
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function(email) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+        },
+        message: 'Please enter a valid email address'
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
 );
 
 // virtrual for friendCount
-
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length
+})
 
 const User = model('User', userSchema);
 
