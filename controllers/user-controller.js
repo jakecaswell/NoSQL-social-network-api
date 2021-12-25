487447443865526a24d5e5b83e58639b4a4ee284
jcaswell <jakecaswell88@gmail.com>
2021-12-25 17:48:47 -0500

    almost finished all of the crud methods for the users. fixed bug that wouldn't pull single user from db

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,7 +6,7 @@ const userController = {
   // I left get all as an example
 
   // get all users
-  getUsers(req, res) {
+  getAllUsers(req, res) {
     User.find()
       .select('-__v')
       .then((dbUserData) => {
@@ -18,17 +18,61 @@ const userController = {
       });
   },
   // get single user by id
-  
+  getUser({ params }, res) {
+    User.findOne({ _id: params.userId })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => {
+        // if no user with this id send 404
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id D:' })
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
   // create a new user
-  
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.status(400).json(err))
+  },
   // update a user
-  
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true })
+      .then(dbUpdatedUser => {
+        if (!dbUpdatedUser) {
+          res.status(404).json({ message: 'No user found with this id D:' })
+          return;
+        }
+        res.json(dbUpdatedUser);
+      })
+      .catch(err => res.status(400).json(err));
+  },
   // delete user (BONUS: and delete associated thoughts)
-  
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.userId })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id D:' })
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
   // add friend to friend list
-  
+  addFriend() {
+
+  },
   // remove friend from friend list
-  
+  deleteFriend() {
+
+  }
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,26 @@
 const router = require('express').Router();
-
+const {
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
 
 // /api/users
-
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
 
 // /api/users/:userId
-
+router
+    .route('/:userId')
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 
